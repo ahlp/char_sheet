673845349fdb78d5a529a70d479a3da5d1a32c9d
Arthur Pires <arthur.pires@nubank.com.br>
2020-06-10 20:12:07 -0300

    Adding dart lint

--- lib/widgets/my_home.dart
+++ lib/widgets/my_home.dart
@@ -29,7 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
